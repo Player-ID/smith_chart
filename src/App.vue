34be88c1e270771e1a80f9a7ec46274f6b19b64d
Daniel Gao <dani3l.gao@gmail.com>
2018-03-14 17:20:47 -0400

    Move code to corresponding files

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,46 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
-  </div>
+  <Chart :cursors="cursors"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import Chart from './components/Chart'
+
+import * as calc from './js/calculations.js'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Chart
+  },
+  data: () => {
+    return {
+      maxCursors: 10,
+      isFull: false,
+      cursors: []
+    }
+  },
+  mounted () {
+    this.spawnCursor()
+  },
+  methods: {
+    spawnCursor () {
+      this.isFull = this.cursors.length + 1 > this.maxCursors
+      if (this.isFull) return
+
+      const defaultResistance = 1
+      const defaultReactance = 0
+
+      var cursor = {}
+      cursor.i = this.cursors.length + 1
+      cursor.resistance = calc.calculateResistanceCircle(defaultResistance)
+      cursor.reactance = calc.calculateReactanceCircle(defaultReactance)
+      cursor.gamma = calc.calculateGamma(defaultResistance, defaultReactance)
+
+      this.cursors.push(cursor)
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/js/calculations.js
@@ -0,0 +1,76 @@
+import * as math from 'mathjs'
+import { CENTER, UNIT_RADIUS } from './constants.js'
+
+function calculateResistanceCircle (value) {
+  if (value < 0) {
+    window.alert('Resistance can\'t be negative!')
+  } else if (value === Infinity) {
+    return {
+      value: value,
+      cx: CENTER + UNIT_RADIUS,
+      cy: CENTER,
+      r: 1
+    }
+  }
+
+  var cx = CENTER + UNIT_RADIUS * value / (value + 1)
+  var cy = CENTER
+  var r = Math.abs(UNIT_RADIUS / (value + 1))
+
+  return {
+    value: value,
+    cx: cx,
+    cy: cy,
+    r: r
+  }
+}
+
+function calculateReactanceCircle (value) {
+  if (Math.abs(value) === Infinity) {
+    return {
+      value: value,
+      cx: CENTER + UNIT_RADIUS,
+      cy: CENTER,
+      r: 1
+    }
+  }
+
+  var cx = CENTER + UNIT_RADIUS
+  var cy = CENTER - UNIT_RADIUS / value
+  var r = Math.abs(UNIT_RADIUS / value)
+
+  return {
+    value: value,
+    cx: cx,
+    cy: cy,
+    r: r
+  }
+}
+
+function calculateGamma (r, x) {
+  if (r === Infinity || Math.abs(x) === Infinity) {
+    return {
+      gamma: math.complex(1, 0).toPolar(),
+      x: CENTER + UNIT_RADIUS,
+      y: CENTER
+    }
+  }
+
+  var z = math.complex(r, x)
+  var gamma = math.divide(math.subtract(z, 1), math.add(z, 1)).toPolar()
+
+  var cx = CENTER + UNIT_RADIUS * gamma.r * Math.cos(gamma.phi)
+  var cy = CENTER - UNIT_RADIUS * gamma.r * Math.sin(gamma.phi)
+
+  return {
+    gamma: gamma,
+    x: cx,
+    y: cy
+  }
+}
+
+export {
+  calculateGamma,
+  calculateReactanceCircle,
+  calculateResistanceCircle
+}
